6dca8e9cfeefb75ec7b701f942f3c749f30ddcda
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-10-03 23:40:43 +0300

    lab6

---
+++ inheritance/lib/src/main/java/inheritance/Main.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Review review1 = new Review("GREAT", "SOME GUY", 3);
+        Review review2 = new Review("BAD", "OSAID", 10);
+        Restaurant restaurant1 = new Restaurant("KFC", "high");
+        restaurant1.addition(review1);
+        restaurant1.addition(review2);
+        System.out.println(restaurant1);
+//        System.out.println(restaurant1.getReviews());
+
+
+
+    }
+}
